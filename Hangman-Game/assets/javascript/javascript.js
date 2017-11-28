bba921d3ea33ae2fa82f4f8a51c3b774a15d8acb
Paul M Herman <pmherman@Pauls-MacBook-Pro-15.local>
2017-11-28 18:33:35 -0500

    Fixed loop

--- Hangman-Game/assets/javascript/javascript.js
+++ Hangman-Game/assets/javascript/javascript.js
@@ -31,67 +31,72 @@
 			var userInput = event.key.toLowerCase();
 
 			if (alphabet.indexOf(userInput) > -1 && lives > 0 && guessesRemaining > 0 ) {
-			
-			   if (randomWord.indexOf(userInput) > -1) {
-				for (var i = 0; i < randomWord.length; i++) {
-					if (randomWord[i] === userInput) {
-						blanks[i] = userInput;
-						lettersUsed.push(userInput);
-						console.log(lettersUsed);
+				
+				if (randomWord.indexOf(userInput) > -1 && lettersUsed.indexOf(userInput) == -1) {
+					for (var i = 0; i < randomWord.length; i++) {
+						if (randomWord[i] === userInput) {
+							blanks[i] = userInput;
+							lettersUsed.push(userInput);
+							console.log(lettersUsed);
+						}
+						document.getElementById("wordHolder").innerHTML = blanks.join(" ");
+						document.getElementById("guessesRemain").innerHTML = "You have " + guessesRemaining + " guesses remaining.";
 					}
-					document.getElementById("wordHolder").innerHTML = blanks.join(" ");
+				}  else if (guesses.indexOf(userInput) > -1 && lettersUsed.indexOf(userInput) > -1) {
+						alert("You have already used this letter");
+						console.log(guesses);
+						guessesRemaining++;
+						lives++;
+
+				}  else if (lettersUsed.indexOf(userInput) > -1) {
+						alert("You have already used this letter");
+						guessesRemaining++;
+				} else {
+						guesses.push(userInput);
+						lettersUsed.push(userInput);
+						document.getElementById("usedLetters").innerHTML = guesses.join(" ");
+						document.getElementById("guessesRemain").innerHTML = "You have " + guessesRemaining + " guesses remaining.";
+
+				}		
+					
+				if (randomWord.indexOf(userInput) === -1) {
+					guessesRemaining--;
+					lives--;
+					document.getElementById("guessesRemain").innerHTML = "You have " + guessesRemaining + " guesses remaining.";
+					document.getElementById("lives").innerHTML = "You have " + lives + " lives remaining.";
+				}
+
+				if (randomWord.indexOf(userInput) > -1) {
+					guessesRemaining--;
 					document.getElementById("guessesRemain").innerHTML = "You have " + guessesRemaining + " guesses remaining.";
 				}
-			}	else if (guesses.indexOf(userInput) > -1 || randomWord.indexOf(userInput) > -1) {
-					alert("You have already used this letter");
-					guessesRemaining++;
-					lives++;
 
-			}	else {
-					guesses.push(userInput);
-					document.getElementById("usedLetters").innerHTML = guesses.join(" ");
-					document.getElementById("guessesRemain").innerHTML = "You have " + guessesRemaining + " guesses remaining.";
+				if (lives === 0) {
+					alert("Game Over! You Lost!");
+					loss++;
+					document.getElementById("loss").innerHTML = "Losses: " + loss;
+				}if (guessesRemaining === 0) {
+					alert("You have run out of guesses! You Lost!");
+					loss++;				
+					document.getElementById("loss").innerHTML = "Losses: " + loss;
+				}if (blanks.indexOf("_ ") === -1) {
+					function myFunction() {
+	    			setTimeout(function(){ alert("Congrats, You Win!!"); }, 500);
+	    		}
+					myFunction();
+					wins++;
+					document.getElementById("wins").innerHTML = "Wins: " + wins;
+				} 
 
-			}		
-				
-			if (randomWord.indexOf(userInput) === -1) {
-				guessesRemaining--;
-				lives--;
-				document.getElementById("guessesRemain").innerHTML = "You have " + guessesRemaining + " guesses remaining.";
-				document.getElementById("lives").innerHTML = "You have " + lives + " lives remaining.";
 			}
-
-			if (randomWord.indexOf(userInput) > -1) {
-				guessesRemaining--;
-				document.getElementById("guessesRemain").innerHTML = "You have " + guessesRemaining + " guesses remaining.";
+			else if (lives == 0 || guessesRemaining == 0) {
+				alert("The Game is over. Please start again");
 			}
-
-			if (lives === 0) {
-				alert("Game Over! You Lost!");
-				loss++;
-				document.getElementById("loss").innerHTML = "Losses: " + loss;
-			}if (guessesRemaining === 0) {
-				alert("You have run out of guesses! You Lost!");
-				loss++;				
-				document.getElementById("loss").innerHTML = "Losses: " + loss;
-			}if (blanks.indexOf("_ ") === -1) {
-				function myFunction() {
-    			setTimeout(function(){ alert("Congrats, You Win!!"); }, 500);
-    		}
-				myFunction();
-				wins++;
-				document.getElementById("wins").innerHTML = "Wins: " + wins;
+			else {
+				alert("Please enter a valid letter!!");
 			}
 
 		}
-		else if (lives == 0 || guessesRemaining == 0) {
-			alert("The Game is over. Please start again");
-		}
-		else {
-			alert("Please enter a valid letter!!");
-		}
-
-		  }
 
 	var music =document.getElementById("song");
 	music.play();
@@ -117,18 +122,6 @@ function gameStart() {
 	console.log(randomWord);
 };
 
-function photoHint() {
-	if (randomWord == "defiant") {
-		document.getElementById("hint").src="assets/images/ds9.png";
-	}
-	if (randomWord == wordBank[3]) {
-		document.getElementById("hint").scr="assets/images/ds9.png";
-	}
-
-	else {
-		document.getElementById("hint").src="assets/images/startrek.png";
-	}
-};
 
 
 
