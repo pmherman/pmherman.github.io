e9d429fe30d70ad542f05c97f6647e7f797bcc33
Paul M Herman <>
2017-12-18 21:42:47 -0500

    upload

--- giphy/assets/javascript/java.js
+++ giphy/assets/javascript/java.js
@@ -1,4 +1,15 @@
-var gifs = ["Star Trek", "DragonBall Z", "Code Geass", "Gundam Seed", "Family Guy", "Designated Survivor"];
+var gifs = ["Star Trek", "DragonBall Z", "Code Geass", "Gundam Seed", "Family Guy", "Designated Survivor", "Gundam Wing", "The Originals", "Supergirl", "The Flash", "Legends of Tomorrow", "Big Bang Theory", "Fresh off the Boat", "Die Hard", "Castle", "Spiderman"];
+
+//Creates the footer at the bottom of the page
+function footer() {
+	var foot = $("<footer>");
+
+	foot.addClass("footer text-");
+	foot.html("<p>Copyright 2017. All Rights Reserved");
+
+	$("body").append(foot);
+}
+
 
 function buttons() {
 
@@ -13,7 +24,7 @@ function buttons() {
 		$("#buttons").append(b);
 	}
 }
-
+	//Adds additional buttons as needed for increased search results
 	$("#buttonAdd").on("click", function(event) {
 		event.preventDefault();
 
@@ -34,7 +45,7 @@ function buttons() {
 		console.log("Clicked");
 
 		var gifButton = $(this).attr("gif-type");
-
+		//Holds the Giphy API information with a variable for the button
 		var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gifButton + "&api_key=dc6zaTOxFJmzC&limit=10";
 
 		$.ajax({
@@ -46,26 +57,57 @@ function buttons() {
 
 			var results = response.data;
 
+			//Creates the for loop that grabs the button names from the array.
 			for (var i=0; i < results.length; i++) {
 				if (results[i].rating !== "r") {
+					//creates the gifDisplay div to house the giphy
 					var gifDisplay = $("<div class='col-md-4'>");
-
+					//grabs the rating
 					var rating = results[i].rating;
 
 					var p = $("<p>").text("Gif Rating: " + rating);
 
-					var buttonImage = $("<img id='gifImage'>");
-
-					buttonImage.attr("src", results[i].images.fixed_width_downsampled.url);
-
+					var buttonImage = $("<img>");
+					//creates the buttons and adds the class
+					buttonImage.addClass("gifImage");
+					//Default animated image
+					buttonImage.attr("src", results[i].images.fixed_width_downsampled.url) ;
+					buttonImage.attr("data-state", "animate");
+					//Still image that can be called on click
+					buttonImage.attr("data-still", results[i].images.fixed_width_still.url);
+					buttonImage.attr("data-animate", results[i].images.fixed_width_downsampled.url);					
+					//addes the gif to the DOM
 					gifDisplay.html(p);
 					gifDisplay.append(buttonImage);
 
 					$("#gifs").prepend(gifDisplay);
 				}
-			}
+			}			
 
 		});
+		$(document).on("click", ".gifImage", function(){
+			//creates the dynamic clicking event for stopping and starting the gif
+			event.preventDefault();
+
+			$(".gifImages").empty();
+
+			var state = $(this).attr("data-state");
+
+			if (state === "animate") {
+				$(this).attr("src", $(this).attr("data-still"));
+				$(this).attr("data-state", "still");
+				console.log("still");
+			} else if (state === "still") {
+				$(this).attr("src", $(this).attr("data-animate"));
+				$(this).attr("data-state", "animate");
+				console.log("animate");
+			}				
+		})			
 
 	})
+
+//Calls my functions 	
 buttons();
+footer();
+
+
