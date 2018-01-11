f58f56d9cb0f297237e9fc72887afae107c45cb2
Paul M Herman <pmherman@Pauls-MacBook-Pro-15.local>
2018-01-11 18:24:57 -0500

    no message

--- testproject2/assets/javascript/app.js
+++ testproject2/assets/javascript/app.js
@@ -1,27 +1,40 @@
+function tplawesome(e,t){res=e;for(var n=0;n<t.length;n++){res=res.replace(/\{\{(.*?)\}\}/g,function(e,r){return t[n][r]})}return res}
+
 $(function() {
-	$("form").on("submit", function(e) {
-		e.preventDefault();
-
-		var request = gapi.client.youtube.search.list( {
-			part: "snippet",
-			type: "video",
-			q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
-			maxResults: 5,
-			order: "viewCount",			
-		});
-
-		request.execute(function(response) {
-			var results = response.result;
-			$.each(results.item, function(index, item) {
-				$("#results").append(item.id.videoID+" "+item.snippet.title+"<br>");
-			});
-		});
-	});
+    $("form").on("submit", function(e) {
+       e.preventDefault();
+       // prepare the request
+       var request = gapi.client.youtube.search.list({
+            part: "snippet",
+            type: "video",
+            q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
+            maxResults: 3,
+            order: "viewCount",
+            publishedAfter: "2015-01-01T00:00:00Z"
+       }); 
+       // execute the request
+       request.execute(function(response) {
+          var results = response.result;
+          $("#results").html("");
+          $.each(results.items, function(index, item) {
+            $.get("tpl/item.html", function(data) {
+                $("#results").append(tplawesome(data, [{"title":item.snippet.title, "videoid":item.id.videoId}]));
+            });
+          });
+          resetVideoHeight();
+       });
+    });
+    
+    $(window).on("resize", resetVideoHeight);
 });
 
-function init() {
-	gapi.client.setAPIKey("AIzaSyAwbv5Uageg_qwYxm898r4e4Eh5eEP6LjU");
-	gapi.client.load("youtube", "v3", function() {
+function resetVideoHeight() {
+    $(".video").css("height", $("#results").width() * 9/16);
+}
 
-	});
+function init() {
+    gapi.client.setApiKey("YOUR_PUBLIC_KEY");
+    gapi.client.load("youtube", "v3", function() {
+        // yt api is ready
+    });
 }
