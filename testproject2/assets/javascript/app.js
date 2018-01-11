c682dfeb3248fac5e201cdd652e31657759ba543
Paul M Herman <pmherman@Pauls-MacBook-Pro-15.local>
2018-01-11 18:58:27 -0500

    no message

--- testproject2/assets/javascript/app.js
+++ testproject2/assets/javascript/app.js
@@ -1,39 +1,22 @@
 function tplawesome(e,t){res=e;for(var n=0;n<t.length;n++){res=res.replace(/\{\{(.*?)\}\}/g,function(e,r){return t[n][r]})}return res}
 
-$(function() {
-    $("form").on("submit", function(e) {
-       e.preventDefault();
-
-
-      })
-  });
 
 
 
-// $(function() {
-//     $("form").on("submit", function(e) {
-//        e.preventDefault();
-//        // prepare the request
-//        var request = gapi.client.youtube.search.list({
-//             part: "snippet",
-//             type: "video",
-//             q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
-//             maxResults: 3,
-//             order: "viewCount",
-//             publishedAt: "2015-01-01T00:00:00Z"
-//        }); 
-       // execute the request
 $(function() {
     $("form").on("submit", function(e) {
        e.preventDefault();
        // prepare the request
-       var request =  buildApiRequest('GET',
-                    '/youtube/v3/search',
-                    {'maxResults': '25',
-                     'part': 'snippet',
-                     'q': encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
-                     'type': 'video'});
-       });        
+       var request = gapi.client.youtube.search.list({
+            part: "snippet",
+            type: "video",
+            q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
+            maxResults: 3,
+            order: "viewCount",
+            publishedAt: "2015-01-01T00:00:00Z"
+       }); 
+       execute the request
+       
        request.execute(function(response) {
           var results = response.result;
           $("#results").html("");
