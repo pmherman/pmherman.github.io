5fb4bfe4eb1b409c2bf20380888c0036e8ed96df
Paul M Herman <pmherman@Pauls_MacBookPro_15.lan>
2018-01-28 22:01:23 -0500

    added log support, switched to switch-case, added functions

--- liri-node-app/liri.js
+++ liri-node-app/liri.js
@@ -6,30 +6,33 @@ var twitter = require("twitter");
 var spotify = require("node-spotify-api");
 var request = require("request");
 var keys = require("./keys.js");
-
-
+var searchInput = "";
+if (process.argv.length > 2) {
+    for (var i = 3; i < process.argv.length; i++) {
+        searchInput += process.argv[i] + "+";
+    }
+}
 //Load client keys
 var spotify = new spotify(keys.spotify);
 var client = new twitter(keys.twitter);
-
-var userInput = process.argv[2]; //option for user response
-var searchInput = process.argv; //input selector for movie or song search
-var status = []; //hold tweets
-var twitterScreenName = {screenname: "pmherman85", count: 20};
-
-//Twitter call
-if (userInput == "my-tweets") {
+//Function to call Twitter API
+var twitterCall = function() {
+    var status = []; //hold tweets
+    var twitterScreenName = {screenname: "pmherman85", count: 20};
     client.get("statuses/user_timeline", twitterScreenName, function(err, tweets, response) {
         for (i = 0; i < tweets.length; i++) {
-            status.push(tweets[i].text);
-        }
+            status.push({
+                "Tweet: ": tweets[i].text,
+                "Created at: ": tweets[i].created_at,
+        });
+    }
         console.log("\nHere are the twitter responses: ");
         //prints to command line
         for (var j = 0; j < status.length; j++) {
             console.log("\nTweet " + j + ": " + JSON.stringify(status[j], null, 2));
 
         }
-        fs.appendFile("tweets.txt","\n" + status, function(err) {
+        fs.appendFile("log.txt","\n" + status + "\n", function(err) {
             if (err) {
                 console.log(err);
             } else {
@@ -37,42 +40,38 @@ if (userInput == "my-tweets") {
             }
         })
     });
-//Spotify Call
-} else if (userInput == "spotify-this-song") {
-    var input = "";
-    for (var j=3; j < searchInput.length; j++) {
-        input = input + searchInput[j];
-    }
-        spotify.search({ type: 'track', query: input }, function(err, data) {
-        if (err) {
-            return console.log('Error occurred: ' + err);
-        } 
-        console.log(JSON.stringify(data, null, 2));
-        })
-//OMDB Call                
-} else if (userInput == "movie-this") {
-    var input = "";
-    if (searchInput[3] == undefined) {
-        input = "Mr + Nobody"; 
-    } else {
-        for (var j = 3; j < searchInput.length; j++) {
-            if (j > 3 && j < searchInput.length) {
-                input = input + "+" + searchInput[j];
-            } else if (j = 3) {
-                input += searchInput[j];
-            } 
-        }
-    }
-    var queryUrl = "http://www.omdbapi.com/?t=" + input + "&y=&plot=short&apikey=trilogy";
+}
+//Function to call Spotify API
+var spotifyCall = function(searchInput) {
 
+    spotify.search({ type: 'track', query: searchInput }, function(err, data) {
+    if (err) {
+        return console.log('Error occurred: ' + err);
+    } 
+    console.log(JSON.stringify(data, null, 2));
+    })
+}
+//Function to call OMDB
+var omdbCall = function(searchInput) {
+    if (searchInput == "") {
+        searchInput = "Mr+Nobody";
+    }
+    var queryUrl = "http://www.omdbapi.com/?t=" + searchInput + "&y=&plot=short&apikey=trilogy";
     request(queryUrl, function(err, response, body) {
-
+        //Function for Rotten Tomates Rating
+        var movieRatings = function() {
+            if (JSON.parse(body).Ratings.length < 2 ) {
+                console.log("Rotten Tomates Rating: No Rating Available");
+            } else {
+                console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+            }
+        }       
         if (!err && response.statusCode === 200) {
             console.log("\n=======================\n");
             console.log("Movie Title: " + JSON.parse(body).Title);
             console.log("Year: " + JSON.parse(body).Year);
             console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
-            console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings); /*Can't get this working!!!!*/
+            movieRatings(); //Rotten Tomates Rating
             console.log("Country: " + JSON.parse(body).Country);
             console.log("Language: " + JSON.parse(body).Language);
             console.log("Actor(s): " + JSON.parse(body).Actors);
@@ -80,7 +79,7 @@ if (userInput == "my-tweets") {
             console.log("\n=======================\n");       
         }
         if (!err && response.statusCode === 200) {
-            fs.appendFile("tweets.txt", "\nYour last movie search was: " + JSON.parse(body).Title, function(err) {
+            fs.appendFile("log.txt", "\nYour last movie search was: " + JSON.parse(body).Title, function(err) {
                 if (err) {
                     console.log(err);
                 } else {
@@ -88,6 +87,45 @@ if (userInput == "my-tweets") {
                 }
             });    
         }
-
     })
 }
+var doWhatItSays = function(searchInput) {
+    fs.readFile("random.txt", "utf8", function(err, data) {
+        if (err) {
+            return console.log(err);
+        }
+        var outputArray = data.split(",");
+        if (outputArray.length == 2) {
+            select(outputArray[0], outputArray[1]);
+        } else {
+            select(outputArray[0]);
+        }
+    })
+    console.log("Name: " + searchInput);
+}
+
+//User Selector
+var select = function(caseInfo) {
+    switch (caseInfo) {
+        case "my-tweets":
+            twitterCall();
+            break;
+        case "spotify-this-song":
+            spotifyCall(searchInput);
+            break;
+        case "movie-this":
+            omdbCall(searchInput);
+            break;
+        case "do-what-it-says":
+            doWhatItSays();
+            break;
+        default:
+            console.log("Unknown Selection, please try again!");
+    }
+} 
+//Executes
+var execute = function(arg1, arg2) {
+    select(arg1, arg2);
+}
+//Take in user input
+execute(process.argv[2], process.argv[3]);
